dd0effe0d80c47dd49336913ab047ea883a0a379
JONAHKIPKORIR <jonahkiplimo1998@gmail.com>
2025-01-31 02:44:30 +0300

    Hide Api Key

--- movie-app-frontend/src/services/api.js
+++ movie-app-frontend/src/services/api.js
@@ -1,5 +1,4 @@
-const API_KEY="a43102dcd22db9eace9c6b21f4194b84";
-// const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDMxMDJkY2QyMmRiOWVhY2U5YzZiMjFmNDE5NGI4NCIsIm5iZiI6MTczODI0MTc5NS4xMjIsInN1YiI6IjY3OWI3NzAzYjAwZDNiYWQ5MmJkN2U1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lvbezbjhiuBu5jE5qBL8vONAZiSYFuSDj80yNW4LRIM";
+const API_KEY="";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
